352bef4f6c1fb512d3fa0467a07e3caab927a8b4
Aditya <aditya.bonde1152@gmail.com>
2023-02-22 00:49:04 +0530

    Token Created and Tests done

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,7 +4,52 @@ pragma solidity ^0.8.17;
 import "../node_modules/hardhat/console.sol";
 
 contract Token{
-    string public name = "mEth";
+    string public name;
     string public symbol = "mEth";
+    uint256 public decimals = 18;
+    uint256 public totalSupply = 1000000 * (10**decimals); // 1000000 * 10^8 Total Supply of mEth Token
+    
+
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
+
+    event Transfer(
+        address indexed from,
+        address indexed to,
+        uint256 _totalSupply
+    );
+
+    constructor( string memory _name , string memory _symbol ,
+    uint256 _decimals , uint256 _totalSupply
+    
+    ){
+            name = _name;
+            symbol = _symbol;
+            totalSupply = _totalSupply;
+            decimals = _decimals;
+
+            balanceOf[msg.sender] = totalSupply;
+       
+    }
+
+    function transfer(address _to , uint256 _value) public returns (bool success) {
+
+        require(balanceOf[msg.sender] >= _value);
+        require(_to != address(0));
+        //Deduct tokens from sender 
+        balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
+        //Credit tokens to reciver
+        balanceOf[_to] =  balanceOf[_to] + _value;
+        emit Transfer(msg.sender , _to , _value);
+        return true;
+
+    }
+
+    function approve(address _spender , uint256 _value) public returns  (bool success) {
+        
+        allowance[msg.sender][ _spender ] = _value;
+        
+    }
+
     
 }
--- test/Token.js
+++ test/Token.js
@@ -1,28 +1,62 @@
 const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
-describe('Token' , () =>{
-    it('has a name' , async () =>{
-        //Fetching Token from Blockchain
-        const Token = await ethers.getContractFactory('Token')
-        let token =  await Token.deploy()
-        //Reading Token Name
-        const name = await token.name();
+const tokens = (n)=>{
 
-        //Checking if name is correct
-        expect(name).to.equal('mEth')
+    return ethers.utils.parseUnits( n.toString() , 'ether')
+
+}
+
+describe('Token' ,() =>{
+
+    let token , accounts , deployer , receiver;
+
+        //Fetching Token from Blockchain
+        beforeEach(async ()=>{
+            const Token = await ethers.getContractFactory('Token')
+           token = await Token.deploy()
+
+            accounts = await ethers.getSigners()
+            deployer = accounts[0]
+            receiver = accounts[1]
+        })
     
+    it('has a name' , async () =>{
+
+        const name = await token.name();
+        expect(name).to.equal('mEth')
+
+    })
+    it('has correct symbol' , async () =>{
+
+        const symbol = await token.symbol();
+        expect(symbol).to.equal('mEth')
+
     })
 
-    it('has a symbol' , async () =>{
-        //Fetching Token from Blockchain
-        const Token = await ethers.getContractFactory('Token')
-        let token =  await Token.deploy()
-        //Reading Token Name
-        const symbol = await token.symbol();
+    it('has correct decimal' , async () =>{
 
-        //Checking if name is correct
-        expect(symbol).to.equal('mEth')
-    
+        const decimals = await token.decimals();
+        expect(decimals).to.equal('18')
+
+    })
+
+    it('has correct total supply' , async () =>{
+
+        const value = tokens('1000000')
+        expect(await token.totalSupply()).to.equal(value)
+
+    })
+
+    describe('Sending Token', () =>{
+
+        it('Transfer Token balances', async ()=>{
+        
+        
+        //Transfer Token
+       token.connect(deployer).tranfer()
+        //Ensure tokens were transfered 
+        })
     })
 })
+
